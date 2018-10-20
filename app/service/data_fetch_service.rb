0b1c5717562fe9239ddd590fdcaa69436ee66d4f
曾文建 <wen@cengwenjiande-MacBook-Pro.local>
2018-10-20 19:33:33 +0800

    modified data_fetch_service

--- app/service/data_fetch_service.rb
+++ app/service/data_fetch_service.rb
@@ -7,14 +7,13 @@ module DataFetchService
 
         def self.execute
             count = 0
-            @period = (365*1.5).round
+            @period = (365 * 1.5).round
             options = Selenium::WebDriver::Chrome::Options.new
             options.add_argument('--headless')
-            driver = Selenium::WebDriver.for :chrome, options: options
+            @driver = Selenium::WebDriver.for :chrome#, options: options
             
             # set window size using Dimension struct
-            target_size = Selenium::WebDriver::Dimension.new(1024, 768)
-            driver.manage.window.size = target_size
+            @driver.manage.window.resize_to(1200, 768)
             
             while count < @period
                 # if count % 20 == 1 && count > 1
@@ -22,69 +21,99 @@ module DataFetchService
                 #     driver = Selenium::WebDriver.for :chrome
                 # end
                 date = Date.tomorrow + count
-                url = "https://flights.wingontravel.com/tickets-oneway-tpe-tyo/?outbounddate=#{date.day}%2F#{date.month}%2F#{date.year}&adults=1&children=0&infants=0&direct=false&cabintype=Tourist&airlinecode=&dport=&aport=&searchbox=t&curr=TWD"
-                driver.navigate.to url
+                url = "https://flights.wingontravel.com/tickets-oneway-tpe-lon/?outbounddate=#{date.day}%2F#{date.month}%2F#{date.year}&adults=1&children=0&direct=false&cabintype=tourist&dport=&aport=&airline=&searchbox=t&curr=TWD"
+                @driver.navigate.to url
 
                 # pause for 20 seconds to avoid being detected as a robot
-                # sleep(8)
+                # sleep(3)
 
                 # set window size using Dimension struct
-                target_size = Selenium::WebDriver::Dimension.new(1024, 768)
-                driver.manage.window.size = target_size
+                @driver.manage.window.resize_to(1200, 768)
 
-                content = nil
-                while content.blank?
-                    serch_btn = driver.find_elements(:xpath, '//*[@id="app"]/div/div[1]/div[1]/header/div/div/div[2]/div[6]/button')[0]
+                content_type_1 = nil
+                content_type_2 = nil
+                while content_type_1.blank? && content_type_2.blank?
+                    serch_btn = @driver.find_elements(:xpath, '//*[@id="app"]/div/div[1]/div[1]/header/div/div/div[2]/div[6]/button')[0]
                     puts serch_btn.text.encode('UTF-8')
                     serch_btn.click
                     sleep(2)
                     # press search button
-                    content = driver.find_elements(:xpath, '//*[@id="app"]/div/div[3]/div/div[2]/div/div[5]')[0]
-                    puts content
+                    content_type_1 = @driver.find_elements(:xpath, '//*[@id="app"]/div/div[3]/div/div[2]/div/div[4]/div')[0]
+                    puts(content_type_1.try(:text))
+                    content_type_2 = @driver.find_elements(:xpath, '//*[@id="app"]/div/div[3]/div/div[2]/div/div[5]/div[1]')[0]
+                    puts(content_type_2.try(:text))
                 end
 
-                # # close advertisement
-                # if count == 0 # || (count % 20 == 1 && count > 1)
-                #     driver.find_elements(:xpath, '//*[@id="wg-site-apppop"]/div/a')[0].click
-                #     driver.find_elements(:xpath,'//*[@id="app"]/div/div[4]/div/a')[0].click
-                # end
+                # close advertisement
+                if count == 0 # || (count % 20 == 1 && count > 1)
+                    @driver.find_elements(:xpath, '//*[@id="wg-site-apppop"]/div/a')[0].click
+                    @driver.find_elements(:xpath,'//*[@id="app"]/div/div[4]/div/a')[0].click
+                end
 
-                # # pause for 8 seconds to avoid being detected as a robot
-                # sleep(2)
+                # pause to avoid being detected as a robot
+                sleep(2)
 
-                # # sorting data according price
-                # (driver.find_elements(:xpath, '//*[@id="app"]/div/div[3]/div/div[2]/div/div[4]/div[4]'))[0].click
-                # # pause for 8 seconds to avoid being detected as a robot
-                # sleep(1)
-
-                # # screening for the flights without transfer
-                # element = driver.find_elements(:xpath, '//*[@id="app"]/div/div[3]/div/div[1]/div/div[2]/div[2]/div/div[1]/div[1]/label/span[2]')[0]
-                # if element.text == "直飛"
-                #     element.click
-                # end
-
-                # sleep(2)
+                # sorting data according price
+                (@driver.find_elements(:xpath, '//*[@id="app"]/div/div[3]/div/div[2]/div/div[3]/div[4]'))[0].click
+                # pause for 8 seconds to avoid being detected as a robot
+                sleep(1)
 
 
-                # find the information of company
-                flight_company = driver.find_elements(:xpath, '//*[@id="app"]/div/div[3]/div/div[2]/div/div[3]/div[3]/div[1]/div/div[1]/span')[0].text.encode('UTF-8')
+
+                if content_type_1.present? && content_type_1.text != '出發時間'
+                    # screening for the flights without transfer
+                    element = @driver.find_elements(:xpath, '//*[@id="app"]/div/div[3]/div/div[1]/div/div[2]/div[2]/div/div[1]/div[1]/label/span[2]')[0]
+                    if element.present? && element.try(:text) == "直飛"
+                        element.click
+                        sleep(2)
+                    end
+                    # find the information of company
+                    debugger
+                    flight_company = @driver.find_elements(:xpath, '//*[@id="app"]/div/div[3]/div/div[2]/div/div[4]/div[1]/div[1]/div[1]/div[2]/div[1]/span/span')[0].present? ? 
+                        @driver.find_elements(:xpath, '//*[@id="app"]/div/div[3]/div/div[2]/div/div[4]/div[1]/div[1]/div[1]/div[2]/div[1]/span/span')[0].text.encode('UTF-8') : 
+                        @driver.find_elements(:xpath, '//*[@id="app"]/div/div[3]/div/div[2]/div/div[5]/div[1]/div[1]/div[1]/div[2]/div[1]/span/span')[0].text.encode('UTF-8')
+                                                                    
+                    # find flight information
+                    price_context = (@driver.find_elements(:xpath, '//*[@id="app"]/div/div[3]/div/div[2]/div/div[4]/div[1]')[0].text)
+                    prices = price_context.scan(/TWD(\d*,\d*)/)
+                    
+                elsif content_type_2.present?
+                    # screening for the flights without transfer
+                    element = @driver.find_elements(:xpath, '//*[@id="app"]/div/div[3]/div/div[1]/div/div[2]/div[2]/div/div[1]/div[1]/label/span[2]')[0]
+                    if element.present? && element.try(:text) == "直飛"
+                        element.click
+                        sleep(2)
+                    end
+                    # find the information of company
+                    flight_company = @driver.find_elements(:xpath, '//*[@id="app"]/div/div[3]/div/div[2]/div/div[5]/div[1]/div[1]/div[1]/div[2]/div[1]/span/span')[0].text.encode('UTF-8')
     
-                # find flight information
-                price = (driver.find_elements(:xpath, '//*[@id="app"]/div/div[3]/div/div[2]/div/div[3]/div[3]/div[1]/div/div[2]/span')[0].text)
-                price = price.gsub('TWD', '') 
-                price = price.gsub(',', '').to_i
+                    # find flight information
+                    price_context = (@driver.find_elements(:xpath, '//*[@id="app"]/div/div[3]/div/div[2]/div/div[5]/div[1]')[0].text)
+                    prices = price_context.scan(/TWD(\d*,\d*)/)
+                end
 
-                ticket = FlightTicket.find_by(flight_date: date)
-                creatable = false
+                lowest_price = 99999999
+                price = 0
+                if prices.present?
+                    prices = prices.flatten
+                    prices.each do |p|
+                        price = p.gsub(',', '').to_i
+                        lowest_price = price if price < lowest_price
+                    end
+                end
+
+                ticket = FlightTicket.find_by(flight_date: date, destination: 'LON')
+                creatable = true
                 if ticket.present?
-                    if ticket.price > price
+                    if ticket.price > lowest_price
                         ticket.update_attributes(
                             flight_company: flight_company,
                             price: 3000
                         )
+                        creatable = false
                     end
-                else
-                    creatable = true
+                elsif prices.nil?
+                    creatable = false
                 end
 
                 if creatable
@@ -131,5 +160,10 @@ module DataFetchService
                 return 12
             end
         end
+
+        def get_flight_information(content)
+            
+        end
+
     end
 end
