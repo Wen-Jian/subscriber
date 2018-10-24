adcbd7002f5a282eef2d55b0782efe1f87958915
曾文建 <wen@cengwenjiande-MacBook-Pro.local>
2018-10-24 22:39:43 +0800

    fix CSRF

--- app/api/api_base.rb
+++ app/api/api_base.rb
@@ -1,5 +1,11 @@
 module API
     class ApiBase < Grape::API
+        before do 
+            headers['Access-Control-Allow-Origin'] = '*'
+            headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+            headers['Access-Control-Request-Method'] = '*'
+            headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+        end
         mount Api::V1::SubscriberApi::ApiV1
     end
     
